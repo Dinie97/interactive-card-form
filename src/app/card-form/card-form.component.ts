49d59a7645e668095df4be682e6ba0ab583ced26
Dinie Syahirah <dinie.syahirah@fullode.com>
2024-06-27 14:49:33 +0800

    add submitted UI

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -9,7 +9,7 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CardFormComponent {
   cardForm: FormGroup;
-
+  submitted: boolean = false;
   constructor() {
     this.cardForm = new FormGroup({
       Name: new FormControl('', Validators.required),
@@ -35,6 +35,6 @@ export class CardFormComponent {
   }
 
   Submit() {
-    console.log(this.cardForm.value);
+    this.submitted = true;
   }
 }
